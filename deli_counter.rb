339abae653bfb4c571d623b1896e999e89932f05
Kordell Parrish <parrishkordell@gmail.com>
2020-08-02 19:41:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,8 @@ katz_deli = []
 
 def line(queue)
   if queue.size > 0 
-    
+        queue.each_with_index do |a, idx|
+    "the line is currently #{idx + 1}. #{a}"
     puts "The line is currently empty."
   elsif queue.size > 0 
 
