2e2614d6ac577a3736b80f891dc5e781428afb0d
Kordell Parrish <parrishkordell@gmail.com>
2020-08-02 19:39:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,6 @@ def line(queue)
     puts "The line is currently empty."
   elsif queue.size > 0 
     queue.each_with_index do |a, idx|
-      "the line is currently #{idx + 1}. #{a}"
+    "the line is currently #{idx + 1}. #{a}"
   end 
 end
